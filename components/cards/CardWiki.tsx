dcc4135ed7fa6ae414e6e97b1ffae6a7eccdd7c1
Vinicius Yuji Nishioka <yuu.nishioka@outlook.com>
2025-04-02 16:44:20 -0300

    feat: link de card no wiki

--- components/cards/CardWiki.tsx
+++ components/cards/CardWiki.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 import Html from "@/components/html/Html";
 
 export default function CardWiki({ data }: any) {
   return (
-    <div className="flex flex-col p-4 gap-2 bg-secondary/30 rounded-lg">
+    <Link
+      href={`/wiki/${data.title}`}
+      className="flex flex-col p-4 gap-2 bg-secondary/30 rounded-lg"
+    >
       <div className="flex items-center justify-between">
         <h2 className="text-lg font-bold">{data.title}</h2>
         <p className="px-2 py-1 text-sm text-white bg-black/80 rounded-lg">
@@ -12,6 +16,6 @@ export default function CardWiki({ data }: any) {
       {data?.text && (
         <Html text={data.text} className="text-sm text-justify text-black/70" />
       )}
-    </div>
+    </Link>
   );
 }
